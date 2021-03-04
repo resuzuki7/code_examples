e887f3585dc6edbdfbfddbccb82e80ac76d84738
resuzuki7 <80011087+resuzuki7@users.noreply.github.com>
2021-03-03 22:19:14 -0500

    scrapper

---
+++ scrapper/beauty-scrapper.py
@@ -0,0 +1,32 @@
+# Web scrapper to get the list of beauty shots from IWCOA Marketing portal
+
+# import requests
+# Skipping getting the actual file.
+# This file is obtained by exporting the HTML of the page via inspect.
+# This cant be done through a direct request at the moment
+
+from bs4 import BeautifulSoup
+
+URL = open("html/beauty-shots.html")
+soup = BeautifulSoup(URL, 'html.parser')
+
+
+results = soup.find_all(class_="modelno")
+
+filter = ''.join([chr(i) for i in range(1, 32)])
+# used to remove the escape characters from strings
+
+beauty_list = []
+
+import csv
+
+for result in results:
+    if None in result:
+        continue
+    results_clean = result.text.translate(str.maketrans('', '', filter))
+    beauty_list.append(results_clean)
+
+with open('beatufylist.csv', 'w', newline='\n') as fileloc:
+    writer = csv.writer(fileloc, delimiter=',')
+    for each in beauty_list:
+        writer.writerow([each])
